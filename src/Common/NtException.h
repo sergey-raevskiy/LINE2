4c212367eb96be0fec171e56cb67a3aff7cd5398
ccik <ccik@47049513-7c0a-b048-97bb-9ae337c634a3>
2013-06-09 18:49:09 +0000

    Added macro NT_ERR_E for automatic NTSTATUS->NtException mapping.
    git-svn-id: http://fallen:8080/svn/LINE2/trunk@7 47049513-7c0a-b048-97bb-9ae337c634a3

--- src/Common/NtException.h
+++ src/Common/NtException.h
@@ -10,3 +10,14 @@ public:
 private:
     NTSTATUS m_Status;
 };
+
+// A helper macro for NTSTATUS wrapping.
+
+#define NT_ERR_E(Expr) do {                                                   \
+    NTSTATUS Status = ( Expr );                                               \
+                                                                              \
+    if (!NT_SUCCESS(Status))                                                  \
+    {                                                                         \
+        throw NtException(Status)                                             \
+    }                                                                         \
+} while (0)
