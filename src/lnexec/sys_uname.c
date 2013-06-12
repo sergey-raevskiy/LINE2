53e31436b793f301c467602621b9470410712471
ccik <ccik@47049513-7c0a-b048-97bb-9ae337c634a3>
2013-06-12 14:14:02 +0000

    Fixed "FATAL: Kernel too old"
    git-svn-id: http://fallen:8080/svn/LINE2/trunk@42 47049513-7c0a-b048-97bb-9ae337c634a3

--- src/lnexec/sys_uname.c
+++ src/lnexec/sys_uname.c
@@ -6,7 +6,7 @@ int sys_uname(linux_new_utsname_t *utsname)
 {
     strncpy(utsname->sysname, "Linux", __NEW_UTS_LEN);
     strncpy(utsname->nodename, "LINE2", __NEW_UTS_LEN);
-    strncpy(utsname->release, "LINE2", __NEW_UTS_LEN);
+    strncpy(utsname->release, "3.5.0-17-generic", __NEW_UTS_LEN);
     strncpy(utsname->version, "LINE2", __NEW_UTS_LEN);
     strncpy(utsname->machine, "x86", __NEW_UTS_LEN);
     strncpy(utsname->domainname, "example.com", __NEW_UTS_LEN);
