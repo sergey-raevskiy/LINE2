c85e0597987a26d0584185af01143e3b541f3360
ccik <ccik@47049513-7c0a-b048-97bb-9ae337c634a3>
2013-06-18 18:13:50 +0000

    Added usage.
    git-svn-id: http://fallen:8080/svn/LINE2/branches/elf-converter@68 47049513-7c0a-b048-97bb-9ae337c634a3

--- src/elf2pe/main.cpp
+++ src/elf2pe/main.cpp
@@ -1,6 +1,22 @@
 #include <stdio.h>
+#include <stdarg.h>
+
+void eprintf(wchar_t *fmt, ...)
+{
+    va_list va;
+
+    va_start(va, fmt);
+    vfwprintf(stderr, fmt, va);
+    va_end(va);
+}
 
 int wmain(int argc, wchar_t **argv)
 {
+    if (argc < 3)
+    {
+        eprintf(L"Usage: elf2pe <input file> <output file>\n");
+        return 1;
+    }
+
     return 0;
 }
